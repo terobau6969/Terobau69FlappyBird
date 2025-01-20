e3b868eaddd6a8518be704a6b9dc71fd37f8a301
Sakar Karki <sakarkarki01@gmail.com>
2025-01-20 16:01:23 +1100

    Flappy Bird CLone V1
    1st version of Flappy Bird Clone by Terobau69

---
+++ app.jsx
@@ -0,0 +1,106 @@
+let config = {
+    renderer: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 300 },
+        debug: false
+      }
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
+  };
+  
+  let game = new Phaser.Game(config);
+  let isGameStarted = false;
+  let bird;
+  let hasLanded =false;
+  let cursors;
+  let hasBumped = false;
+  let messageToPlayer;
+
+  function preload() {
+    this.load.image('background', 'assets/background.png');
+    this.load.image('road', 'assets/road.png');
+    this.load.image('column', 'assets/column.png');
+    this.load.spritesheet('bird', 'assets/bird.png', { frameWidth: 64, frameHeight: 96 });
+  }
+  
+  function create() {
+    const background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+    const roads = this.physics.add.staticGroup();
+    const topColumns = this.physics.add.staticGroup({
+        key: 'column',
+        repeat: 1,
+        setXY: { x: 200, y: 0, stepX: 300 }
+      });
+
+      const bottomColumns = this.physics.add.staticGroup({
+        key: 'column',
+        repeat: 1,
+        setXY: { x: 350, y: 400, stepX: 300 },
+      });
+      
+    const road = roads.create(400, 568, 'road').setScale(2).refreshBody();
+
+    bird = this.physics.add.sprite(0, 50, 'bird').setScale(2);
+    bird.setBounce(0.2);
+    bird.setCollideWorldBounds(true);
+    this.physics.add.overlap(bird, road, () => hasLanded = true, null, this);
+    this.physics.add.collider(bird, road);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.overlap(bird, topColumns, ()=>hasBumped=true,null, this);
+    this.physics.add.overlap(bird, bottomColumns, ()=>hasBumped=true,null, this);
+    this.physics.add.collider(bird, topColumns);
+    this.physics.add.collider(bird, bottomColumns);
+
+    messageToPlayer = this.add.text(0, 0, `Instructions: Press space bar to start`, { fontFamily: '"Comic Sans MS", Times, serif', fontSize: "20px", color: "white", backgroundColor: "black" });
+    Phaser.Display.Align.In.BottomCenter(messageToPlayer, background, 0, 50);
+  }
+  
+  function update() {
+    if (!isGameStarted) {
+        bird.setVelocityY(-160);
+      }
+
+    if (cursors.space.isDown && !isGameStarted) {
+        isGameStarted = true;
+        messageToPlayer.text = 'Instructions: Press the "^" button to stay upright\n          and don\'t hit the columns or ground';
+      }
+    
+    if (cursors.up.isDown && !hasLanded && !hasBumped) {
+      bird.setVelocityY(-160);
+    }
+  
+    if (!hasLanded || !hasBumped) {
+        bird.body.velocity.x = 50;
+      }
+      
+    if (hasLanded || hasBumped || !isGameStarted) {
+        bird.body.velocity.x = 0;
+      }
+
+      if (hasLanded || hasBumped) {
+        messageToPlayer.text = `Oh no! You crashed!`;
+      }
+    
+
+    if (cursors.space.isDown && !isGameStarted) {
+        isGameStarted = true;
+    }
+
+    if (bird.x > 750) {
+        bird.setVelocityY(40);
+        messageToPlayer.text = `Congrats! You won!`;
+      } 
+    
+
+   
+}
